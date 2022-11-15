b7bf62829b89aeba8e00c23cc91f4f928d372f25
Nathan Sankary <nesankar@mtu.edu>
2022-11-14 21:27:16 -0800

    add docstrings

--- mlexpy/utils.py
+++ mlexpy/utils.py
@@ -9,17 +9,60 @@ logger.setLevel(logging.INFO)
 
 
 def make_directory(directory_path: Path) -> None:
+    """Create a directory defiend by the directory path, including creating all intermediate directories.
+
+    Parameters
+    ----------
+    directory_path : Path
+        The full path to the directory you would like to create.
+
+    Returns
+    -------
+    None
+    """
     logger.info(f"Creating the directory(s) at {directory_path}.")
     directory_path.mkdir()
 
 
 def df_assertion(data_structure: Any) -> None:
+    """Simply assert that the passed value is a dataframe type.
+
+    Parameters
+    ----------
+    data_structure : Any
+        The value to check the type of.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    AssertionException
+        If you didn't pass a dataframe
+    """
     assert isinstance(
         data_structure, pd.DataFrame
     ), f"The provided variable is not a pd.DataFrame ({type(data_structure)}). Need to pass a DataFrame."
 
 
 def series_assertion(data_structure: Any) -> None:
+    """Simply assert that the passed value is a Series type.
+
+    Parameters
+    ----------
+    data_structure : Any
+        The value to check the type of.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    AssertionException
+        If you didn't pass a Series
+    """
     assert isinstance(
         data_structure, pd.Series
     ), f"The provided variable is not a pd.Series ({type(data_structure)}). Need to pass a Series."
@@ -28,8 +71,23 @@ def series_assertion(data_structure: Any) -> None:
 def initial_filtering(
     df: pd.DataFrame, column_mask_functions: Dict[str, List[Callable]]
 ) -> pd.DataFrame:
-    """For each column in the initial dataframe, create a masking function to define a boolean to keep, or not keep the record.
+    """For each column in the initial dataframe, create a masking function to
+    define a boolean to keep, or not keep the record.
+
     In the end, pass over every record and keep only the records that are all True.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe you would like to perform filtering on.
+    column_mask_functions : Dict[str, List[Callable]]
+        The Dictionary of masking functions to apply. Each function is only applied to the
+        respective key in the dictionary. The masking functions are by default a list in
+        order to allow multiple functions per column.
+
+    Returns
+    -------
+    pd.DataFrame
     """
 
     initial_size = df.shape
