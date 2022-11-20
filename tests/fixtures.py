6492b186d5078f01c433511d7785870dd8906e08
Nathan Sankary <nesankar@mtu.edu>
2022-11-20 11:10:10 -0800

    fix full regression and classification examples

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -20,6 +20,20 @@ def simple_dataframe() -> pd.DataFrame:
 
 
 @pytest.fixture()
+def simple_binary_dataframe() -> pd.DataFrame:
+    var_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    var_2 = [0, 2, 1, 2, 3, 1, 2, 3, 4, 1]
+
+    target = ["a", "a", "a", "a", "a", "a", "b", "b", "b", "b"]
+
+    return pd.DataFrame(
+        zip(var_1, var_2, target),
+        columns=["obs1", "obs2", "target"],
+        index=list(range(len(var_1))),
+    )
+
+
+@pytest.fixture()
 def simple_series(simple_dataframe) -> pd.Series:
     return simple_dataframe["target"]
 
@@ -27,7 +41,7 @@ def simple_series(simple_dataframe) -> pd.Series:
 @pytest.fixture()
 def base_processor() -> processor.ProcessPipelineBase:
     """Create a base data processor class to test."""
-    return processor.ProcessPipelineBase()
+    return processor.ProcessPipelineBase(store_models=False)
 
 
 @pytest.fixture()
--- tests/test_ml_examples.py
+++ tests/test_ml_examples.py
@@ -3,16 +3,15 @@ from mlexpy import experiment, processor, pipeline_utils
 import pandas as pd
 from pathlib import Path
 from typing import Optional, Union, Callable
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 import sys
 from numpy.testing import assert_array_equal
-from fixtures import simple_dataframe, to_scale_dataframe, rs_10, rs_20
+from fixtures import simple_dataframe, simple_binary_dataframe, rs_10, rs_20
 
 
-def test_regression_model_match(simple_dataframe, rs_10, rs_20):
-
-    # First, crete a processor object
-    class my_processor(processor.ProcessPipelineBase):
+@pytest.fixture
+def basic_processor():
+    class basic_processor(processor.ProcessPipelineBase):
         def __init__(
             self,
             process_tag: str = "_development",
@@ -33,7 +32,6 @@ def test_regression_model_match(simple_dataframe, rs_10, rs_20):
             if training:
                 self.fit_model_based_features(df)
                 model_features = self.transform_model_based_features(df)
-                self.dump_feature_based_models()
             else:
                 # Here we can ONLY apply the transformation
                 model_features = self.transform_model_based_features(df)
@@ -43,11 +41,15 @@ def test_regression_model_match(simple_dataframe, rs_10, rs_20):
         def fit_model_based_features(self, df: pd.DataFrame) -> None:
             for column in df.columns:
                 if not self.check_numeric_column(df[column]):
-                    print(df[column].dtype)
                     continue
                 self.fit_scaler(df[column], standard_scaling=True)
 
-    class my_experiment(experiment.RegressionExperimentBase):
+    return basic_processor
+
+
+@pytest.fixture
+def regression_experiment(basic_processor):
+    class regression_obj(experiment.RegressionExperimentBase):
         def __init__(
             self,
             train_setup: pipeline_utils.MLSetup,
@@ -76,7 +78,7 @@ def test_regression_model_match(simple_dataframe, rs_10, rs_20):
             self, process_method_str: str = "process_data", from_file: bool = False
         ) -> pipeline_utils.ExperimentSetup:
 
-            data_processor = my_processor(
+            data_processor = basic_processor(
                 process_tag=self.process_tag, model_dir=self.model_dir
             )
 
@@ -121,6 +123,94 @@ def test_regression_model_match(simple_dataframe, rs_10, rs_20):
                 ),
             )
 
+    return regression_obj
+
+
+@pytest.fixture
+def classification_experiment(basic_processor):
+    class classification_obj(experiment.ClassifierExperimentBase):
+        def __init__(
+            self,
+            train_setup: pipeline_utils.MLSetup,
+            test_setup: pipeline_utils.MLSetup,
+            cv_split_count: int = 5,
+            rnd_int: int = 100,
+            model_dir: Optional[Union[str, Path]] = None,
+            model_storage_function: Optional[Callable] = None,
+            model_loading_function: Optional[Callable] = None,
+            model_tag: str = "_development",
+            process_tag: str = "_development",
+        ) -> None:
+            super().__init__(
+                train_setup,
+                test_setup,
+                cv_split_count,
+                rnd_int,
+                model_dir,
+                model_storage_function,
+                model_loading_function,
+                model_tag,
+                process_tag,
+            )
+
+        def process_data(
+            self, process_method_str: str = "process_data", from_file: bool = False
+        ) -> pipeline_utils.ExperimentSetup:
+
+            data_processor = basic_processor(
+                process_tag=self.process_tag, model_dir=self.model_dir
+            )
+
+            # Now get the the data processing method defined in process_method_str.
+            process_method = getattr(data_processor, process_method_str)
+
+            # First, determine if we are processing data via loading previously trained transformation models...
+            if from_file:
+                # ... if so, just perform the process_method function for training
+                test_df = process_method(self.testing.obs, training=False)
+                test_labels = data_processor.encode_labels(self.testing.labels)
+
+                return pipeline_utils.ExperimentSetup(
+                    pipeline_utils.MLSetup(
+                        pd.DataFrame(),
+                        pd.Series(),
+                    ),
+                    pipeline_utils.MLSetup(
+                        test_df,
+                        test_labels,
+                    ),
+                )
+            else:
+                train_df = process_method(self.training.obs, training=True)
+                test_df = process_method(self.testing.obs, training=False)
+
+                train_labels = data_processor.encode_labels(self.training.labels)
+                test_labels = data_processor.encode_labels(self.testing.labels)
+
+            print(
+                f"The train data are of size {train_df.shape}, the test data are {test_df.shape}."
+            )
+
+            assert (
+                len(set(train_df.index).intersection(set(test_df.index))) == 0
+            ), "There are duplicated indices in the train and test set."
+
+            return pipeline_utils.ExperimentSetup(
+                pipeline_utils.MLSetup(
+                    train_df,
+                    train_labels,
+                ),
+                pipeline_utils.MLSetup(
+                    test_df,
+                    test_labels,
+                ),
+            )
+
+    return classification_obj
+
+
+def test_regression_model_match(simple_dataframe, rs_10, rs_20, regression_experiment):
+
     # Now do the experiment...
 
     dataset = pipeline_utils.get_stratified_train_test_data(
@@ -129,14 +219,11 @@ def test_regression_model_match(simple_dataframe, rs_10, rs_20):
         test_frac=0.5,
         random_state=rs_20,
     )
-    experiment_obj = my_experiment(
+    experiment_obj = regression_experiment(
         train_setup=dataset.train_data,
         test_setup=dataset.test_data,
-        model_tag="test_regression_model_match_process",
-        process_tag="test_regression_model_match_model",
-        model_dir=Path(
-            __file__
-        ).parent,  # This means I will place the models alongside this script
+        model_tag="regression_match_model",
+        process_tag="regression_match_process",
     )
 
     # Now begin the experimentation, start with performing the data processing...
@@ -179,14 +266,11 @@ def test_regression_model_match(simple_dataframe, rs_10, rs_20):
     )
 
     # Do the same thing with a newly named class
-    new_experiment = my_experiment(
+    new_experiment = regression_experiment(
         train_setup=dataset.train_data,
         test_setup=dataset.test_data,
-        model_tag="test_regression_model_match_process",
-        process_tag="test_regression_model_match_model",
-        model_dir=Path(
-            __file__
-        ).parent,  # This means I will place the models alongside this script
+        model_tag="regression_match_model",
+        process_tag="regression_match_process",
     )
     new_datasets = new_experiment.process_data_from_stored_models()
     new_model = new_experiment.load_model()
@@ -206,5 +290,118 @@ def test_regression_model_match(simple_dataframe, rs_10, rs_20):
         [
             trained_value == new_results[trained_key]
             for trained_key, trained_value in results.items()
+        ]
+    )
+
+
+def test_classification_model_match(
+    simple_binary_dataframe, rs_10, rs_20, classification_experiment
+):
+
+    # Now do the experiment...
+    dataset = pipeline_utils.get_stratified_train_test_data(
+        simple_binary_dataframe[["obs1", "obs2"]],
+        simple_binary_dataframe["target"],
+        test_frac=0.5,
+        random_state=rs_20,
+    )
+    experiment_obj = classification_experiment(
+        train_setup=dataset.train_data,
+        test_setup=dataset.test_data,
+        model_tag="classification_match_model",
+        process_tag="classification_match_process",
+    )
+
+    # Now begin the experimentation, start with performing the data processing...
+    processed_datasets = experiment_obj.process_data()
+
+    # ... then train our model...
+    trained_model = experiment_obj.train_model(
+        RandomForestClassifier(random_state=rs_10),
+        processed_datasets,
+    )
+    # Get the predictions and evaluate the performance.
+    predictions = experiment_obj.predict(processed_datasets, trained_model)
+    results = experiment_obj.evaluate_predictions(
+        processed_datasets,
+        predictions=predictions,
+    )
+
+    # Here we store the model
+    experiment_obj.store_model(trained_model)
+
+    # Now, test that the results are the same if loading from a disk.
+    loaded_datasets = experiment_obj.process_data_from_stored_models()
+    loaded_model = experiment_obj.load_model()
+    # Get the predictions and evaluate the performance.
+    loaded_predictions = experiment_obj.predict(loaded_datasets, loaded_model)
+    loaded_results = experiment_obj.evaluate_predictions(
+        loaded_datasets,
+        predictions=loaded_predictions,
+    )
+
+    # Assert that the predictions are the same
+    assert_array_equal(predictions, loaded_predictions)
+
+    # Assert that the evaluation metrics are all the same
+    assert all(
+        [
+            results[key] == loaded_results[key]
+            for key in [
+                "f1_micro",
+                "f1_macro",
+                "log_loss",
+                "accuracy",
+                "balanced_accuracy",
+                "f1_weighted",
+            ]
+        ]
+    )
+
+    # Do the same thing with a newly named class
+    new_experiment = classification_experiment(
+        train_setup=dataset.train_data,
+        test_setup=dataset.test_data,
+        model_tag="classification_match_model",
+        process_tag="classification_match_process",
+    )
+    new_datasets = new_experiment.process_data_from_stored_models()
+    new_model = new_experiment.load_model()
+
+    # Get the predictions and evaluate the performance.
+    new_predictions = new_experiment.predict(new_datasets, new_model)
+    new_results = new_experiment.evaluate_predictions(
+        new_datasets,
+        predictions=new_predictions,
+    )
+
+    # Assert that the predictions are the same
+    assert_array_equal(predictions, loaded_predictions)
+
+    # Assert that the evaluation metrics are all the same
+    print(
+        [
+            results[key] == new_results[key]
+            for key in [
+                "f1_micro",
+                "f1_macro",
+                "log_loss",
+                "accuracy",
+                "balanced_accuracy",
+                "f1_weighted",
+            ]
+        ]
+    )
+    assert all(
+        [
+            results[key] == new_results[key]
+            for key in [
+                "f1_micro",
+                "f1_macro",
+                "log_loss",
+                "accuracy",
+                "balanced_accuracy",
+                "f1_weighted",
+            ]
         ]
     )
