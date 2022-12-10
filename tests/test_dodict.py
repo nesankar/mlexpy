9a86d0fa19982f78ece7a3d3bdaf67875eb45f48
Nathan Sankary <nesankar@mtu.edu>
2022-12-10 12:58:56 -0800

    add default ordered dict test

---
+++ tests/test_dodict.py
@@ -0,0 +1,31 @@
+import pytest
+from src.defaultordereddict import DefaultOrderedDict
+
+
+def test_initial_value():
+    dod = DefaultOrderedDict(lambda: [])
+
+    # Test that if there is no key provided, the default value is correct.
+    assert dod[0] == []
+
+    dod = DefaultOrderedDict(lambda: 0)
+    # Test that if there is no key provided, the default value is correct.
+    assert dod[0] == 0
+
+
+def test_correct_ordering():
+    dod = DefaultOrderedDict(lambda: 0)
+    keys = ["6", "c", "a", "d", "b"]
+
+    for item in keys:
+        dod[item] += 1
+
+    dod_keys = list(dod.keys())
+
+    # Test that the ordering in the dict is the same as when input into the dict.
+    assert all([kv == dod_keys[i] for i, kv in enumerate(keys)])
+
+    # Test that the values are correct as we retrieve them
+
+    assert dod["6"] == 1
+    assert dod["z"] == 0
